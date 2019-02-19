18f5b45bef16758fb26ca15def31fac235827ba9
jumodada <864353360@qq.com>
2019-02-19 20:02:08 +0800

    重构了一下代码

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,6 @@
 <template>
-<div class="col" :class="[span && `col-${span}`,offset&&`offset-${offset}`]"
-     :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+<div class="col" :class="[colClasses]"
+     :style="colStyle"
 
 >
     <div  style="border:2px solid green;
@@ -28,6 +28,21 @@
                 gutter:0,
             }
         },
+        computed:{
+            colClasses(){
+                let {span,offset} = this
+                return [
+                    span && `col-${span}`,
+                offset && `offset-${offset}`
+            ]
+            },
+            colStyle(){
+                return {
+                    paddingLeft:this.gutter/2 + 'px',
+                    paddingRight:this.gutter/2 + 'px'
+                }
+            }
+        }
 
 
     }
