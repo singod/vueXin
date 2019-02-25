7e1fdca759c7c5f887124d08f07b85e5dc2f3b1e
jumodada <864353360@qq.com>
2019-02-25 20:10:03 +0800

    重构了一下，完成click基本功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,13 +1,72 @@
 <template>
-
+    <div class="popover" @click="onClick" ref="popover">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+            <slot name="content"></slot>
+        </div>
+        <span ref="trigger">
+            <slot></slot>
+        </span>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "popover"
+        name: "g-popover",
+        data () {
+            return {visible: false}
+        },
+
+        methods: {
+            contentPosition(){
+                document.body.appendChild(this.$refs.contentWrapper)
+                let {top,left} = this.$refs.trigger.getBoundingClientRect()
+                this.$refs.contentWrapper.style.left = left + window.screenX + 'px'
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+            },
+            listenToDocument(){
+                document.addEventListener('click',this.eventHandler)
+            },
+            eventHandler(e){
+                if( this.$refs.popover &&!(this.$refs.popover===e.target || this.$refs.popover.contains(e.target))) {
+                    console.log('已关闭')
+                    this.close()
+                }
+            },
+            open(){
+                this.$nextTick(() => {
+                    this.contentPosition() //搞定内容弹出的位置
+                    this.listenToDocument()//添加document的事件监听，在外部点击可以关闭气泡
+                })
+            },
+            close(){
+                this.visible = false
+                document.removeEventListener('click', this.eventHandler)
+            },
+            onClick () {
+                if (this.$refs.trigger.contains(event.target)) {
+                    this.visible = !this.visible
+                    if (this.visible) {
+                        this.open()
+                    }else{
+                        this.close()
+                    }
+                }
+            }
+        },
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.popover{
+    display: inline-block;
+    vertical-align: top;
+    position: relative;
 
+}
+.content-wrapper{
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    transform: translateY(-100%);
+}
 </style>
